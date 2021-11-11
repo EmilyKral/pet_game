26d877edbbbfe5653804a420ae466e8f06bec2ec
EmilyKral <emilykral@gmail.com>
2021-11-11 20:52:07 +0000

    small bug fixes and tidying up the text formatting

--- pet.js
+++ pet.js
@@ -1,4 +1,5 @@
 var prompt = require("prompt-sync")();
+// npm install prompt-sync
 
 class Pet {
 	constructor(name, happiness = 5, hungerLevel = 5) {
@@ -8,53 +9,55 @@ class Pet {
 	}
 
 	playWith() {
-		if (this.happiness >= 10) {
-			console.log(`${this.name} loves playing with you so much`);
+		if (this.happiness === 10) {
+			console.log(`${this.name} is very tired from playing with you so much! They need a break.\n`);
+		} else if (this.hungerLevel === 10) {
+			console.log(`${this.name} doesn't have the energy to play right now! They need some food!\n`);
 		} else {
 			this.happiness++;
-			console.log(`${this.name} is very pleased to play`);
+			this.hungerLevel++;
+			console.log(`${this.name} is very pleased to play.\n`);
 		}
 	}
 
 	feed() {
 		if (this.hungerLevel === 0) {
-			console.log(`${this.name} is full`);
+			console.log(`${this.name} is full, they would prefer to do something else now.\n`);
 		} else {
 			this.hungerLevel--;
-			console.log(`${this.name} really appreciates the food`);
+			console.log(`${this.name} really appreciates the food!\n`);
 		}
 	}
 
 	leaveAlone() {
 		if (this.happiness === 0) {
-			console.log(
-				`Please don't leave ${this.name} alone! They are very sad and would love to play`
-			);
+			console.log(`Please don't leave ${this.name} alone! They are very sad and want to play!\n`);
 		} else if (this.hungerLevel === 10) {
-			console.log(`Please don't leave ${this.name} alone! THey are very hungry`);
+			console.log(`Please don't leave ${this.name} alone! They are very hungry!\n`);
 		} else {
-			this.hungerLevel--;
-			this.happiness--;
+			console.log(`\n \n \n \n \n \n${this.name} comes to find you.\n`);
+			this.hungerLevel++;
+			this.happiness -= 2;
 		}
 	}
 
 	getHappiness() {
 		if (this.happiness < 3) {
-			return `${this.name} is sad`;
-		} else if (this.happiness < 7) {
-			return `${this.name} is doing fine`;
+			return `${this.name} is sad.`;
+		} else if (this.happiness <= 7) {
+			return `${this.name} is doing fine.`;
 		} else {
 			return `${this.name} is really happy!`;
 		}
 	}
 
 	getHungerLevel() {
-		if (this.hungerLevel < 3) {
-			return `${this.name} is not hungry at all`;
-		} else if (this.happiness < 7) {
-			return `${this.name} might need some food soon`;
+		if (this.hungerLevel <= 4) {
+			return `${this.name} is not hungry at all.`;
+		} else if (this.hungerLevel <= 8) {
+			return `${this.name} might need some food soon.`;
 		} else {
-			return `${this.name} is very hungry, please feed them soon!`;
+			return `${this.name} is very hungry, please feed them!`;
 		}
 	}
 }
@@ -68,29 +71,42 @@ function createPet() {
 function interactWithPet() {
 	let pet = createPet();
 	let done = false;
+	let count = 0;
+	let action;
 	while (!done) {
-		let action = prompt(
-			`What would you like to do with ${pet.name}? You can play, feed, check on or leave alone. If you want to stop, please type done. `
-		);
-		if (action === "play") {
+		if (count === 0) {
+			action = prompt(
+				`What would you like to do with ${pet.name}? You can play, feed, check on or leave alone. If you want to stop, please type done. `
+			);
+		} else {
+			action = prompt(`What would you like to do with ${pet.name}? `);
+		}
+		if (action.toLowerCase() === "play") {
 			pet.playWith();
-		} else if (action === "feed") {
+		} else if (action.toLowerCase() === "feed") {
 			pet.feed();
-		} else if (action === "check on") {
+		} else if (action.toLowerCase() === "check on") {
 			let happinessLevel = pet.getHappiness();
 			let hungerLevel = pet.getHungerLevel();
 			console.log(happinessLevel);
-			console.log(hungerLevel);
-		} else if (action === "done") {
-			console.log("Thanks for playing!");
-			done = true;
-		} else if (action === "leave alone") {
+			console.log(hungerLevel + "\n");
+			let furtherInfo = prompt(`Would you like to see more information about ${pet.name}? `);
+			if (furtherInfo.toLowerCase() === "yes") {
+				console.log(
+					`Pet name: ${pet.name}, happiness: ${pet.happiness}/10, hunger: ${pet.hungerLevel}/10\n`
+				);
+			}
+		} else if (action.toLowerCase() === "leave alone") {
 			pet.leaveAlone();
+		} else if (action.toLowerCase() === "done") {
+			console.log(`Thanks for playing with ${pet.name} today! Goodbye!`);
+			done = true;
 		} else {
 			console.log(
-				"Please enter a valid command. You can play, feed, check on or leave alone. If you want to stop, please type done. "
+				"\nPlease enter a valid command. You can play, feed, check on or leave alone. If you want to stop, please type done."
 			);
 		}
+		count++;
 	}
 }
 
